10af0db79308c80fbe7626fe83ff3067b6f38226
tokisuno <lucaswmei@gmail.com>
2025-05-16 19:19:12 -0400

    ts

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,29 @@
 import './styles.css';
 
 // placeholder todo class
-function TodoItem (title: string, desc: string, dueDate: string, priority: string) {
-  this.title = title;
-  this.desc = desc;
-  this.dueDate = dueDate;
-  this.priority = priority;
+class TodoItem {
+  title: string;
+  desc: string;
+  dueDate: string;
+  priority: string;
+
+  constructor(title: string, desc: string, dueDate: string, priority: string) {
+    this.title = title;
+    this.desc = desc;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+class TodoList {
+  lists: Array<TodoItem>;
+
+  constructor(lists: Array<TodoItem>) {
+    this.lists = [...lists]
+  }
 }
 
 // idk what type this has to be yet
-function TodoLists (...list: any) {
-  this.list = [...list]
-}
+// function TodoLists (...list: any) {
+//   this.list = [...list]
+// }
